3a272ab0aaa1b69759b1f6e57afde9998d30f280
hkxia95 <hongkangxia@163.com>
2021-05-04 21:39:18 +0800

    # 4-20 # LC617 # 893、63 # 1011 # sql1082 # 938 # 1518 # 554

--- leetcode554/砖墙/Solution.java
+++ leetcode554/砖墙/Solution.java
@@ -8,7 +8,6 @@ import java.util.Map;
 public class Solution {
 
     //其穿过的砖块数量加上从边缘经过的砖块数量之和是一个定值，即砖墙的高度
-    //
     public int leastBricks(List<List<Integer>> wall) {
         HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
         int res = 0;
@@ -18,7 +17,7 @@ public class Solution {
             int sum = 0;
             for (int i = 0; i < n - 1; i++) {
                 sum += eachWall.get(i);
-                hashMap.put(sum, hashMap.getOrDefault(sum, 0) + 1); // 没有 key 返回默认值
+                hashMap.put(sum, hashMap.getOrDefault(sum, 0) + 1); // 如果 HashMap 中没有该 key，则返回默认值
             }
         }
         for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
