a7a4a4bddfa1fe59161be63db58d1ee59626e022
HaviXia <hongkangxia@163.com>
2021-05-20 11:27:20 +0800

    # 4-20          # LC617          # 893、63          # 1011          # sql1082          # 938          # 1518          # 554          # 29          # 35、69          # 872          # 1583          # 109          # 121、122          # 101          # 1734

--- leetcode1734/解码异或后的排列/Solution.java
+++ leetcode1734/解码异或后的排列/Solution.java
@@ -1,8 +1,24 @@
 package leetcode1734.解码异或后的排列;
 
 
-public class Solution {
+class Solution {
+    // 异或运算解码
     public int[] decode(int[] encoded) {
-        
+        int n = encoded.length + 1;
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            total ^= i;
+        }
+        int odd = 0;
+        for (int i = 1; i < n - 1; i += 2) {
+            odd ^= encoded[i];
+        }
+        int[] perm = new int[n];
+        perm[0] = total ^ odd;
+        for (int i = 0; i < n - 1; i++) {
+            perm[i + 1] = perm[i] ^ encoded[i];
+        }
+        return perm;
     }
+
 }
